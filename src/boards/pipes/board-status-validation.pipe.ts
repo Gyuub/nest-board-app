b4bc5f0cd7a4fd500eb50a0c253a1af541c70cf1
Gyuub <hman0301@kakao.com>
2023-01-03 21:19:18 +0900

    feat: checking the change in a post

---
+++ src/boards/pipes/board-status-validation.pipe.ts
@@ -0,0 +1,22 @@
+import { ArgumentMetadata, BadRequestException, Logger, PipeTransform } from "@nestjs/common";
+import { of } from "rxjs";
+import { BoardStatus } from "../board.model";
+
+/**
+ * BoardStatus Pipe
+ */
+export class BoardStatusValidationPipe implements PipeTransform{
+    transform(value: any) {
+        Logger.log(`value : ${value}`)
+        
+        if(!this.isStatusValid(value)){
+            throw new BadRequestException(`Invalid parameter value. [parameter] : ${value}`);
+        }
+        return value;
+    }
+
+    private isStatusValid(status: string){
+        return Object.keys(BoardStatus).indexOf(status) !== -1 ? true : false;
+    }
+    
+}
